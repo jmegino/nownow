8d5bcace4f30e9af16a15fc819598965d92d1a2f
jmegino <jerald_megino@yahoo.com.ph>
2018-06-29 18:00:52 +0800

    Modified, planning to rollback

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -18,11 +18,21 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}">
+
+    <!--Ionicons-->
+    <link rel="stylesheet" href="{{asset('Ionicons/css/ionicons.min.css')}}">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/skin-mod.css')}}">
+
     <!-- Bootstrap core CSS -->
     <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
 
     <!--external css-->
-    <link href="{{asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet"/>
     <link rel="stylesheet" type="text/css" href="{{asset('css/zabuto_calendar.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('js/gritter/css/jquery.gritter.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{asset('lineicons/style.css')}}">
@@ -35,13 +45,11 @@
 
     <!-- Custom Styles -->
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="awesome-bootstrap.checkbox.css"> --}}
-    <link rel='stylesheet' id='fontawesome-css' href='https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css?ver=cef393249aa859f391433a18b021ac10' type='text/css' media='all' />
-    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"> --}}
+    {{--<link rel='stylesheet' id='fontawesome-css' href='https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css?ver=cef393249aa859f391433a18b021ac10' type='text/css' media='all' />--}}
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
